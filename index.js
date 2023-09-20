6e1506c6529c2f305d3e7675cc9fc26f95fb2147
dharez <mauroromo.developer@outlook.com>
2023-09-19 22:52:45 -0300

    agregando un puerto al backend

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ import express from "express";
 // 1- Configuraciones iniciales
 const app = express();
 app.set("port", process.env.PORT || 4000);
-
+app.listen(app.get("port"), () => {
+  console.log(`Estoy en el puerto ${app.get("port")}`);
+});
 // 2- Middlewares (Son funciones que hacen cosas especificas)
 
 // 3- Crear las rutas
