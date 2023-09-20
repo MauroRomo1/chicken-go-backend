06cc48279678729a7aa25fa852bef8601304214c
dharez <mauroromo.developer@outlook.com>
2023-09-19 22:05:53 -0300

    agregando la configuracion de los modulos

---
+++ index.js
@@ -0,0 +1,8 @@
+import express from "express";
+
+console.log("Esto es una prueba");
+// 1- Configuraciones iniciales
+
+// 2- Middlewares (Son funciones que hacen cosas especificas)
+
+// 3- Crear las rutas
