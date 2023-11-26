810643fbb3a17225654f303292bd836c9ab0785c
dharez <mauroromo.developer@outlook.com>
2023-11-26 01:49:53 -0300

    terminando la funcion para el login de usuarios

--- index.js
+++ index.js
@@ -5,19 +5,15 @@ import "dotenv/config";
 import userRouter from "./src/routes/users.routes.js";
 import "./src/database/database.js";
 
-// 1- Configuraciones iniciales
 const app = express();
 app.set("port", process.env.PORT || 4000);
 app.listen(app.get("port"), () => {
   console.log(`Estoy en el puerto ${app.get("port")}`);
 });
 
-// 2- Middlewares (Son funciones que hacen cosas especificas)
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
-// 3- Crear las rutas
-// http://localhost:4000/api/usuario
 app.use("/api", userRouter);
